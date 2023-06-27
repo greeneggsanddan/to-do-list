9ba612c3d79d126a8184fa61578cf9b392db4c4c
greeneggsanddan <greeneggsanddan@gmail.com>
2023-06-27 14:09:23 -0700

    Add the 'New project' button to the sidebar

--- src/displayController.js
+++ src/displayController.js
@@ -21,7 +21,28 @@ export function displayProjectList(projects) {
         sidebarDiv.appendChild(projectBtn);
     });
 
+    sidebarDiv.appendChild(createNewProjectBtn());
+
     return sidebarDiv;
+}
+
+function createNewProjectBtn() {
+    const button = document.createElement("button");
+    const addSymbol = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    const buttonText = document.createElement("p");
+
+    button.classList.add("sidebar-btn");
+    
+    addSymbol.setAttribute("viewBox", "4 4 16 16");
+    path.setAttribute("d", "M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z");
+    buttonText.textContent = "New project";
+
+    addSymbol.appendChild(path);
+    button.appendChild(addSymbol);
+    button.appendChild(buttonText);
+
+    return button;
 }
 
 export function displayProject(project) {
